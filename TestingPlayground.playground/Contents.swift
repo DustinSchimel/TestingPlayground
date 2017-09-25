788a9eb53b9597b60bec77a5c2e7ade41d38a3c1
Dustin Schimel <dustin.schimel@gmail.com>
2017-09-25 13:48:55 -0600

    Added string usage examples for assignment.

--- TestingPlayground.playground/Contents.swift
+++ TestingPlayground.playground/Contents.swift
@@ -1,5 +1,49 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+var stringVar = "Guy Fieri"     //String Change
+
+stringVar = "is amazing"
+
+
+
+var letterVar = "G"             //String Interpolation
+
+var numberVar = 210
+
+var interpolatedString = "\(letterVar)uy Fieri weighs \(numberVar) pounds"
+
+
+
+//print(stringVar.count)          Counting Characters
+
+
+
+var varOne = "Yummy"            //Comparing Strings
+
+var varTwo = "Yummy"
+
+if varOne == varTwo
+{
+    print("They're the same!")
+}
+else
+{
+    print("They're different!")
+}
+
+
+
+var boolVar = false             //Personal Tests
+
+var i = 0
+
+while boolVar == false
+{
+    i = i + 1
+    print("I am number \(i)")
+    
+    if i >= 10
+    {
+        boolVar = true
+    }
+}
