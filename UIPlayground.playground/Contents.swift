6d6e1646f577c95808f8c1003f36be86fed51ea4
Dustin Schimel <dustin.schimel@gmail.com>
2017-10-04 13:52:16 -0600

    Added buttons with different colors and text

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -1,7 +1,15 @@
 import UIKit
 
-var defaultText = "Wow this is really cool"
+var topic = "Working with GUI"
 
-let rect = CGRect(x: 10, y: 10, width: 100, height: 100)
-let myView = UIView(frame: rect)
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width:600, height:120))
 
+var mySecondButton = UIButton(frame: CGRect(x:60, y:60, width: 100, height:100))
+
+myFirstButton.setTitle("Words", for: .normal)
+
+myFirstButton.backgroundColor = .green
+
+mySecondButton.setTitle("Wow what color", for: .normal)
+
+mySecondButton.backgroundColor = UIColor(red:0.78, green:0.35, blue:0.35, alpha:1.0)
